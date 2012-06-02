8b92939e6eec361b32c294b8c57f5a6264772951
Alexandre Angelim <angelim@angelim.com.br>
2012-06-02 05:16:01 -0300

    migrating to asset pipeline

--- lib/delayed_job_web.rb
+++ lib/delayed_job_web.rb
@@ -1,1 +1,7 @@
 require 'delayed_job_web/application/app'
+
+class DelayedJobRailtie < Rails::Railtie
+  initializer "precompile whoops assets" do |app|
+    app.config.assets.precompile += ["delayed.js", "delayed.css"]
+  end
+end
